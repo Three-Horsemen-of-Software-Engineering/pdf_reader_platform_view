9c7d0e8f7e8c117b7956179e9fd45dea0ff11b97
vzaika <ecw3R7iTyasgraAt>
2019-10-24 19:32:23 +0300

    add gesture recognizer

--- lib/src/pdf_platform_view.dart
+++ lib/src/pdf_platform_view.dart
@@ -1,20 +1,27 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 
 class PdfPlatformView extends StatelessWidget {
   final String path;
 
-  const PdfPlatformView({@required this.path,Key key}) : super(key: key);
+  const PdfPlatformView({@required this.path, Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid) {
       return AndroidView(
-        viewType: "pdf_reader_platform_view",
-        creationParams: <String, String>{"path": path},
-        creationParamsCodec: StandardMessageCodec(),
+          viewType: "pdf_reader_platform_view",
+          creationParams: <String, String>{"path": path},
+          creationParamsCodec: StandardMessageCodec(),
+          gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+            new Factory<OneSequenceGestureRecognizer>(
+                  () => new EagerGestureRecognizer(),
+            ),
+          ].toSet(),
       );
     } else {
       throw UnimplementedError("Ios is not implemented");

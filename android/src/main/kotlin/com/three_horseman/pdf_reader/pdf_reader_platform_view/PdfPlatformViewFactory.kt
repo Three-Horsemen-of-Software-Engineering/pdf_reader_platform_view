824bff2f03117b507155fe6d1f60400d7bbf99e3
vzaika <ecw3R7iTyasgraAt>
2019-11-05 12:20:58 +0200

    remove method

--- android/src/main/kotlin/com/three_horseman/pdf_reader/pdf_reader_platform_view/PdfPlatformViewFactory.kt
+++ android/src/main/kotlin/com/three_horseman/pdf_reader/pdf_reader_platform_view/PdfPlatformViewFactory.kt
@@ -1,11 +1,19 @@
 package com.three_horseman.pdf_reader.pdf_reader_platform_view
 
 import android.content.Context
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class PdfPlatformViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class PdfPlatformViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE){
+
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: PluginRegistry.Registrar) {
+            registrar.platformViewRegistry().registerViewFactory("pdf_reader_platform_view", PdfPlatformViewFactory())
+        }
+    }
 
     override fun create(context: Context?, id: Int, args: Any?): PlatformView {
         if (args == null) {

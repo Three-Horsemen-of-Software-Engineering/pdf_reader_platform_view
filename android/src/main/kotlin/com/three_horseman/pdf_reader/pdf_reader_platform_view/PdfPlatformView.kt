68c3835c99e83fee2e8cda357f08047e9193e0be
vzaika <ecw3R7iTyasgraAt>
2019-11-05 13:28:45 +0200

    toast test

--- android/src/main/kotlin/com/three_horseman/pdf_reader/pdf_reader_platform_view/PdfPlatformView.kt
+++ android/src/main/kotlin/com/three_horseman/pdf_reader/pdf_reader_platform_view/PdfPlatformView.kt
@@ -2,21 +2,25 @@ package com.three_horseman.pdf_reader.pdf_reader_platform_view
 
 import android.content.Context
 import android.view.View
-import com.github.barteksc.pdfviewer.PDFView
+import android.widget.Button
+import android.widget.Toast
 import io.flutter.plugin.platform.PlatformView
-import java.io.File
 
 open class PdfPlatformView(private val context: Context?, private val path: String) : PlatformView {
     override fun dispose() {
     }
 
     override fun getView(): View {
-        return PDFView(context, null).apply {
-            fromFile(File(path))
-                    .enableSwipe(true)
-                    .swipeHorizontal(false)
-                    .enableDoubletap(true)
-                    .defaultPage(0).load()
+        return Button(context).apply {
+            text = "button"
+            setOnClickListener { Toast.makeText(context, "Button was pressed", Toast.LENGTH_SHORT).show() }
         }
+//        return PDFView(context, null).apply {
+//            fromFile(File(path))
+//                    .enableSwipe(true)
+//                    .swipeHorizontal(false)
+//                    .enableDoubletap(true)
+//                    .defaultPage(0).load()
+//        }
     }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(child: PdfPlatformView(path: null,)),
+        body: Center(child: PdfPlatformView(path: null,gestureRecognizers: null,)),
       ),
     );
   }
